24c56b7c6fa0fa8b930f9ff0c6853c2e1d6ae83e
ElenaKas8 <149805926+ElenaKas8@users.noreply.github.com>
2023-12-14 10:39:33 +0100

    14.12 function

---
+++ Lesson3-12.12 Practica/Lesson4/lesson.js
@@ -0,0 +1,16 @@
+//Function declaration
+
+// function double(num) {
+//   return num ** 2;
+// }
+
+//*Стрелочная функйия
+
+// let double = (num) => num ** 2;
+//
+// console.log(double(5));
+
+//*Пример 2 более 2 ух строк кода
+let double = (num) => {
+  return num ** 2; //обязаны написать return,так как ⁡⁢⁢⁢фигурные скобки⁡
+};
