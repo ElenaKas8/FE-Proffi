f487b1548f5caa87e63ee9b4d9ed30169e886a48
Elena <36508011+Ferst1@users.noreply.github.com>
2023-12-15 11:54:35 +0100

    updated

---
+++ summery 15.12/my.js
@@ -0,0 +1,33 @@
+//*callback function */
+
+// function hi(){
+//     console.log('hi!');
+// }
+
+// function hello(){
+//     console.log('hello!');
+// }
+
+
+// function welcome(callback) {
+//     callback();
+//     console.log('You are welcome!');
+// }
+
+
+// welcome(hi)
+
+// function render (){
+//     return"render Word"
+// }
+// render()
+
+
+//const result = console.log('hello')
+
+
+
+
+
+
+
