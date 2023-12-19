3f1cc2df17f0528c07eefa5bd7c1d0b7ddffc167
Elena <elenakaspruk80@gmail.com>
2023-12-19 11:42:08 +0100

    includes

--- lesson5/my.js
+++ lesson5/my.js
@@ -1,6 +1,43 @@
 //*Lesson closures-замыкание
 
-function handle() {
-    let a = 10 + 50;
-    return a;
+// function handle(h) {
+//     let a = 2;
+//     let b = 10;
+//     return function (m) {
+//         console.log(a,m);
+//     };
+// }
+// let b = handle(5);
+// let c = handle(10);
+
+// b(1);   
+// b(4);
+
+//-----------------------------------------
+
+// function generateUrl(zone){
+//     return function(domen){
+//         console.log(`${domen}.${zone}`);
+
+//     }
+// }
+// let comUrl = generateUrl("com");
+// let netUrl = generateUrl("net");
+// comUrl("google");
+// comUrl("amazon");
+
+// netUrl("test");
+// netUrl("ebay");
+
+
+// Напиши функцию pow(n), которая в качестве аргумента будет принимать значение степени. 
+// Функция должна вернуть другую функцию, которая в качестве аргумента будет принимать число num и выводить в коноль 
+// результат вычисления степени (num ** 2)
+function pow(n) {
+    return  (num) => console.log(num ** n);
+    
 }
+let square = pow(2);
+square(3);
+square(4);
+square(5);
---
+++ lesson5/string.js
@@ -0,0 +1,18 @@
+//методы строк
+
+// let string = 'hello world';
+
+// string.toUpperCase();
+// string.toLowerCase();
+//возвращают новую строку
+
+//индексация строк
+
+// let string = 'hello world';
+// console.log(string[3]);
+
+//------------------
+//--includes(substring) --поиск подстроки в строке
+
+'Привет мой друг'.includes('мир');
+
