e2ea2b78557a8fc6080953c7a0aca8ffe1331c4a
Elena <elenakaspruk80@gmail.com>
2023-12-19 11:52:54 +0100

    startWith,endWith

--- lesson5/string.js
+++ lesson5/string.js
@@ -13,6 +13,17 @@
 
 //------------------
 //--includes(substring) --поиск подстроки в строке
+//метод возвращает true или false булевый тип
 
-'Привет мой друг'.includes('мир');
+// 'Привет мой друг';
+
+// //   startWith(substring)проверяет начало  строки
+// //   endWith(substring)проверяет конец строки
+// let string = 'Привет мой друг';
+// console.log(string.startsWith('Привет'));
+// console.log(string.startsWith('мой'));
+// //
+
+// console.log(string.endsWith('друг'));
+
 
