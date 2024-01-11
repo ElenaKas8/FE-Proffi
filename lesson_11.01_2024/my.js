6ab9cae4bdd31743784f8764fd8e75596d3a9786
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-11 09:56:01 +0100

    filter-task

---
+++ lesson_11.01_2024/my.js
@@ -0,0 +1,15 @@
+//Filter
+//Task
+// let users = [
+//     {id: 1, age: 30, name: 'Alex'},
+//     {id: 2, age: 20, name: 'Steven'},
+//     {id: 3, age: 40, name: 'Neena'},
+//     {id: 4, age: 50, name: 'John'},
+//     {id: 5, age: 30, name: 'Grend'}
+// ]
+
+// Сформируйте новый массив, который будет хранить пользователей с возрастом от 30 до 40 лет включительно
+
+// let result = users.filter(user => user.age >= 30 && user.age <= 40)
+// console.log(result)
+
