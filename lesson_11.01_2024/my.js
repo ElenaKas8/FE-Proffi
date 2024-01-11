08f3b97e246445b8994c4cce7762c62966b656af
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-11 10:23:10 +0100

    some,every

--- lesson_11.01_2024/my.js
+++ lesson_11.01_2024/my.js
@@ -12,12 +12,31 @@
 
 // let result = users.filter(user => user.age >= 30 && user.age <= 40)
 // console.log(result)
-
+//----------------------------------------------
 //  find()-метод позволяет найти элемент массива по условию
 
-//метод возвращает найденный элемент массива
+//метод возвращает найденный элемент массива 
 //если метод не найдет по условию элемент ,он вернет undefined
 //ищет слева направо и возвращает первое правильное значение
+// let array = [11,22,13,4,5,66,7,8,9]
+// let result = array.find(elem => elem >15)
+// console.log(result);
+//---------------------------------
+
+//findIndex()-метод позволяет найти индекс элемента массива по условию
+//если не найден возвращает -1*как indexOf
+// let array = [11,22,13,4,5,66,7,8,9]
+// let result = array.findIndex((elem )=> elem >15)
+// console.log(result);
+//-----------------------------------
+
+// some() && every()
+// some()-проверяет наличие ХОТЯ БЫ ОДНОГО ЭЛЕМЕНТЫ по условию и возвращает булевое значение, если находит элемента в массиве
+// every -проверяет все ли элементы в массиве удовлетворяют условию и возвращает булевое значение
+// возвращают boolean
 let array = [11,22,13,4,5,66,7,8,9]
-let result = array.find(elem => elem > 15)
-console.log(result);
+let someResult = array.some(elem => elem >15)
+let everyResult = array.every(elem => elem >15)
+console.log(someResult)
+console.log(everyResult)
+
