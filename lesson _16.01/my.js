0dbef50a882cc52c6d7f0769c1031a36a220833b
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-16 10:59:01 +0100

    let,spread operator

---
+++ lesson _16.01/my.js
@@ -0,0 +1,73 @@
+//методы массивов
+
+// Метод Reduce()
+//метод который позволяет рассчитывать аналитические задачи*среднее число,сумма значение,)
+//метод возвращает итоговое значение аккумулятора
+//  let array = [1,2,3,4,5]
+//посчитать сумму значений в массиве
+// поиск суммы без метода reduce
+// let sum=0;//сохраняется промежуточное значение
+// for  (let elem of array) {
+//      sum += elem
+  
+// }
+// console.log(sum)
+
+
+// let array = [1,2,3,4,5]
+// let sum = array.reduce((acc,elem) => acc + elem)
+// console.log('sum',sum);
+// //получить среднее значение масива
+
+// console.log(sum/array.length);
+
+// c методом reduce()
+// let array = [1,2,3,4,5]
+// let sum = array.reduce((accum, elem) => accum + elem / array.length, 0) 
+
+// console.log(sum);
+
+//Task
+// Посчитайте в одной переменной произведеное, в другой сумму значений в массиве numbers
+
+//  let numbers = [3,3,3,3,4]
+
+//  let sum = numbers.reduce((accum, elem) => accum * elem, 1)
+//  let sum2 = numbers.reduce((accum, elem) => accum + elem, 0)
+//  console.log(sum,sum2);
+ 
+//посчитать сумму всех возрастов массива
+
+
+//  let users = [
+//      {id: 4, age: 30, name: 'Alex'},
+//         {id: 5, age: 20, name: 'Steven'},
+//         {id: 2, age: 40, name: 'Neena'},
+//         {id: 1, age: 50, name: 'John'},
+//         {id: 3, age: 30, name: 'Grend'}
+//     ]
+
+// let sum = users.reduce((accum, elem) => accum + elem.age, 0)
+// console.log(sum)
+
+// // 2. Рассчитать средний возраст сотрудников
+// let middleAge = users.reduce((accum, elem) => accum + elem.age, 0) / users.length
+// console.log(middleAge);
+
+// spread оператор - распаковка массива в указанном месте.
+//  let array = [1,2,3,4,5]
+// // console.log(...array)
+// //---------------------------------Math.max
+// console.log('max',Math.max(...array));
+
+//rest operator - деструктуризация,когда обявляем переменные одной строчкой кода
+
+let array = [10,20,30,40,]
+//или let [a,b,c,d] = [10,20,30,40]
+// let a array [1]
+// let b array [2] 
+// let c array [3]
+// let d array [4]
+
+let [a,b,] = array
+console.log(a,b);
--- summery_12.01/summery.js
+++ summery_12.01/summery.js
@@ -3,7 +3,7 @@
 
 // let array = [11,22,13,4,5,66,7,8,9]
 // array.forEach(elem => console.log(elem))
-const numbers = [5, 2, 1, 9, -6];
+// const numbers = [5, 2, 1, 9, -6];
 //вывести в консоль только положительные числа
 numbers.forEach((elem) => (elem > 0 ? console.log(elem) : null));
 
@@ -16,9 +16,9 @@ numbers.forEach((elem) => (elem > 0 ? console.log(elem) : null));
 //
 //Filter()-возвращает новый массив из элементов которые удовлетворяют условию
 
-const numbers = ["5", "2", 2, 1, 9, "fghj", -6];
+// const numbers = ["5", "2", 2, 1, 9, "fghj", -6];
 
-numbers.sort((a, b) => {
-  return a.charCodeAt(0) - b.charCodeAt(0);
-});
-console.log("numbers", numbers);
+// numbers.sort((a, b) => {
+//   return a.charCodeAt(0) - b.charCodeAt(0);
+// });
+// console.log("numbers", numbers);
